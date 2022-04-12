29d8998dbdf5e9654742be017cccd55e5fec9bd3
Рома Данилов <35975332+Nekiplay@users.noreply.github.com>
2022-04-12 19:44:53 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static Encoding encoding = Encoding.Default;
+        static void Main(string[] args)
+        {
+            Console.Title = "File Cryptor by Neki_play";
+            Console.WriteLine("Доступные действия:\n1. Зашифровать\n2. Расшифровать");
+            Console.Write("Ваше действие: ");
+            string ans = Console.ReadLine();
+            Console.Write("Пароль: ");
+            string pass = Console.ReadLine();
+            Console.Write("Путь до файла: ");
+            string file = Console.ReadLine().Replace("\"", "").Replace("'", "");
+            if (ans == "1")
+            {
+                CryptFile(file, pass, "Neki");
+            }
+            else
+            {
+                UncryptFile(file, pass, "Neki");
+            }
+        }
+
+        static void CryptFile(string file, string password, string salt)
+        {
+            byte[] bytes = File.ReadAllBytes(file);
+            string text = encoding.GetString(bytes, 0, bytes.Length);
+            Cryptor cryptor = new Cryptor();
+            Reader reader = new Reader(file, ' ', encoding);
+            string crypted = cryptor.Crypt(text, password, salt);
+            reader.WriteBytesToFile(crypted);
+        }
+        static void UncryptFile(string file, string password, string salt)
+        {
+            Reader reader = new Reader(file, ' ', encoding);
+            string text = reader.ReadBytesFromFile();
+            Cryptor cryptor = new Cryptor();
+            string crypted = cryptor.Decrypt(text, password, salt);
+            File.WriteAllBytes(file, encoding.GetBytes(crypted));
+        }
+        
+    }
+}
---
+++ ConsoleApp1/Reader.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Reader
+    {
+        private string path { get; }
+        private Encoding encoding = Encoding.Unicode;
+        private char writer = ' ';
+        public Reader(string path, char writer, Encoding encoding)
+        {
+            this.path = path;
+            this.writer = writer;
+            this.encoding = encoding;
+        }
+
+        public string ReadBytesFromFile()
+        {
+            if (File.Exists(path))
+            {
+                List<byte> byt = new List<byte>();
+                using (StreamReader reader = new StreamReader(path))
+                {
+                    string text = reader.ReadToEnd();
+                    foreach (string a in text.Split(writer))
+                    {
+                        byt.Add(Convert.ToByte(a));
+                    }
+
+                    reader.Close();
+                }
+                string readed = encoding.GetString(byt.ToArray(), 0, byt.Count);
+                return readed;
+            }
+            else
+            {
+                return string.Empty;
+            }
+        }
+        public void WriteBytesToFile(string text)
+        {
+            if (!File.Exists(path))
+                File.Create(path).Close();
+            byte[] aa = encoding.GetBytes(text);
+            File.WriteAllText(path, string.Join(writer.ToString(), aa));
+        }
+    }
+}
